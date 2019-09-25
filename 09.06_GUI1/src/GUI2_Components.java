7881fed1e240012fc32292d8e5845ad7cf7d17cb
ssep4u <ssep4u@gmail.com>
2019-09-25 09:28:08 +0900

    make showResult()

--- 09.06_GUI1/src/GUI2_Components.java
+++ 09.06_GUI1/src/GUI2_Components.java
@@ -46,34 +46,24 @@ public class GUI2_Components {
         bt0.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                String result = "";
-                result += lb0.getText();
-                result += bt0.getText();
-                result += cb0.isSelected();
-                result += cb1.isSelected();
-                result += rb0.isSelected();
-                result += rb1.isSelected();
-                result += tf0.getText();
-                result += ta0.getText();
-                result += cbox0.getSelectedIndex();
-                result += cbox0.getSelectedItem();
-                lbResult.setText(result);
+                showResult(lb0, bt0, cb0, cb1, rb0, rb1, tf0, ta0, cbox0, lbResult);
             }
         });
         ItemListener il = new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
-                if(e.getSource() == cb0) {
-                    System.out.println("JCheckbox0 : "+cb0.isSelected());
-                } else if(e.getSource() == cb1) {
-                    System.out.println("JCheckbox1 : "+cb1.isSelected());
-                } else if(e.getSource() == rb0) {
-                    System.out.println("미성년자 : "+rb0.isSelected());
-                } else if (e.getSource() == rb1) {
-                    System.out.println("성인 : "+rb1.isSelected());
-                } else if (e.getSource() == cbox0) {
-                    System.out.println(cbox0.getSelectedItem());
-                }
+                showResult(lb0, bt0, cb0, cb1, rb0, rb1, tf0, ta0, cbox0, lbResult);
+//                if(e.getSource() == cb0) {
+//                    System.out.println("JCheckbox0 : "+cb0.isSelected());
+//                } else if(e.getSource() == cb1) {
+//                    System.out.println("JCheckbox1 : "+cb1.isSelected());
+//                } else if(e.getSource() == rb0) {
+//                    System.out.println("미성년자 : "+rb0.isSelected());
+//                } else if (e.getSource() == rb1) {
+//                    System.out.println("성인 : "+rb1.isSelected());
+//                } else if (e.getSource() == cbox0) {
+//                    System.out.println(cbox0.getSelectedItem());
+//                }
             }
         };
         cb0.addItemListener(il);
@@ -112,5 +102,20 @@ public class GUI2_Components {
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private static void showResult(JLabel lb0, JButton bt0, JCheckBox cb0, JCheckBox cb1, JRadioButton rb0, JRadioButton rb1, JTextField tf0, JTextArea ta0, JComboBox<String> cbox0, JLabel lbResult) {
+        String result = "";
+        result += lb0.getText();
+        result += bt0.getText();
+        result += cb0.isSelected();
+        result += cb1.isSelected();
+        result += rb0.isSelected();
+        result += rb1.isSelected();
+        result += tf0.getText();
+        result += ta0.getText();
+        result += cbox0.getSelectedIndex();
+        result += cbox0.getSelectedItem();
+        lbResult.setText(result);
     }
 }
